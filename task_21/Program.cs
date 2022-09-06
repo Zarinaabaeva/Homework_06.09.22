9e3fc93430a8be191e821e72659511217edbc676
Zarina <abaevazarina@yandex.ru>
2022-09-07 00:31:01 +0300

    changed task_21

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -1,9 +1,21 @@
-﻿int x1 = 7;
-int y1 = -5;
-int z1 = 0;
-int x2 = 1;
-int y2 = -1;
-int z2 = 9;
+﻿// int x1 = 7;
+// int y1 = -5;
+// int z1 = 0;
+// int x2 = 1;
+// int y2 = -1;
+// int z2 = 9;
+
+// double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+
+// Console.WriteLine(result);
+
+Console.WriteLine("Введите x1, y1, z1, x2, y2, z2:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
 
