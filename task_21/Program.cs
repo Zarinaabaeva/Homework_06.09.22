e4f272dbed3a64585d9fac00adad320cb6c37425
Zarina <abaevazarina@yandex.ru>
2022-09-06 23:40:25 +0300

    task_21 solved

---
+++ task_21/Program.cs
@@ -0,0 +1,12 @@
+﻿int x1 = 7;
+int y1 = -5;
+int z1 = 0;
+int x2 = 1;
+int y2 = -1;
+int z2 = 9;
+
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+
+Console.WriteLine(result);
+
+
